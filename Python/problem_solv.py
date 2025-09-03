c7ee649250433cc8ba4c4f724fd3758ca5e5d114
Maisha Osman Umama <umamau418@gmail.com>
2025-09-04 01:14:45 +0600

    kind a disturbing one

--- Python/problem_solv.py
+++ Python/problem_solv.py
@@ -1,6 +1,11 @@
-year = int(input())
-
-if ( year%2==0 and year%100 != 0) or year % 400 == 0 :
-    print(True)
-else:
-    print(False)
+def is_leap(year):
+    if(year %4==0 and year%100 != 0) or(year%400==0):
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
+    
+# to pass the test cases on hackerrank we have to write return 
+# if we want to run the code normally we must call the function 
+# like is_leap(1900)
