7610fc461d1eb872a8441511512fb754f5d20913
Maisha Osman Umama <umamau418@gmail.com>
2025-09-04 02:38:07 +0600

    gatekeeper usage

--- Python/problem_solv.py
+++ Python/problem_solv.py
@@ -1,11 +1,4 @@
-def is_leap(year):
-    if(year %4==0 and year%100 != 0) or(year%400==0):
-        print(True)
-        return True
-    else:
-        print(False)
-        return False
-    
-# to pass the test cases on hackerrank we have to write return 
-# if we want to run the code normally we must call the function 
-# like is_leap(1900)
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i)
